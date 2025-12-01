4a0825d5f18620e98cb87878d927ab57f0b85ceb
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-01 21:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
 
-// Do not change the code below
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+
+// necessary for Cypress
+SportsCar.prototype.constructor = SportsCar;
+
 window.Car = Car;
 window.SportsCar = SportsCar;
